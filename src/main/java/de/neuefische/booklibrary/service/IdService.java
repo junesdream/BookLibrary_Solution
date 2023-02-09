f8480d3c415158bc26a707d215ef62bf1c328f9d
MoinMarcell <marcell.dechant@protonmail.com>
2023-02-09 14:36:44 +0100

    Add service annotation

--- src/main/java/de/neuefische/booklibrary/service/IdService.java
+++ src/main/java/de/neuefische/booklibrary/service/IdService.java
@@ -1,7 +1,10 @@
 package de.neuefische.booklibrary.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.UUID;
 
+@Service
 public class IdService {
     public String generateId(){
         return UUID.randomUUID().toString();
